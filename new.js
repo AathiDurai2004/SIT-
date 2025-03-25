54109fc900fa6b93af2878bd4f0ec90cec63a0d7
AathiDurai2004 <aariyaad2004@gmail.com>
2025-03-25 11:20:20 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("studentCard").addEventListener("click", function () {
-        window.location.href = "student/login.html"; // Redirect to Student Login Page
+        window.location.href = "ES_main_page.html"; // Redirect to Student Login Page
     });
 
     document.getElementById("controllerCard").addEventListener("click", function () {
-        window.location.href = "controller/login.html"; // Redirect to Controller Login Page
+        window.location.href = "controller_main_page.html"; // Redirect to Controller Login Page
     });
 });
